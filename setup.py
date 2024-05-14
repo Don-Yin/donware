b7fa365d7ec4d775fdd65cbde9d79746a81dff07
Don Yin <Don_Yin@outlook.com>
2024-05-14 13:45:00 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="don-ware",
+    version="0.1.0",
+    author="Don Yin",
+    author_email="Don_Yin@outlook.com",
+    description="Don's personal toolkits for data science and machine learning.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Don-Yin/don-ware",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
